66544a1b69b92b26c0fe514edc36dd6d2d1e52f9
zzleomar <dmy.leomar@gmail.com>
2021-02-01 21:08:36 -0400

    d1bd46f

---
+++ _nuxt/static/1612227979/recharge/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002Fepayco2\u002F_nuxt\u002Fstatic\u002F1612227979",layout:"default",error:null,state:{account:{act_account:{}},notification:{spinning:false},pay:{act_pay:{}}},serverRendered:true,routePath:"\u002Frecharge",config:{}};
