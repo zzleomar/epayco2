323c1ec707874f2c029ad9d3ef27ac2fc8c4c8f7
zzleomar <zzleomar@users.noreply.github.com>
2021-02-05 16:22:24 +0000

    deploy: d1bd46f9fa539d11e2dfbf88f1a38c7c9b2ccc6b

--- _nuxt/static/1612227979/recharge/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002Fepayco2\u002F_nuxt\u002Fstatic\u002F1612227979",layout:"default",error:null,state:{account:{act_account:{}},notification:{spinning:false},pay:{act_pay:{}}},serverRendered:true,routePath:"\u002Frecharge",config:{}};
---
+++ _nuxt/static/1612542139/invoice/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002Fepayco2\u002F_nuxt\u002Fstatic\u002F1612542139",layout:"default",error:null,state:{pay:{act_pay:{}},notification:{spinning:false},account:{act_account:{}}},serverRendered:true,routePath:"\u002Finvoice",config:{}};
